46c13a0b3aa007df26b5426af143818d17a0c71c
Exiforce <java.program@mail.r>
2021-12-18 22:09:36 +1000

    2. Сокобан. Тестовое задание. [#75283 #238576]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -6,12 +6,10 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean result = false;
         for (int row = 0; row < board.length; row++) {
-            for (int cell = 0; cell < board[row].length; cell++) {
-                if (board[row][cell] == 1 && (monoHorizontal(board, row)
-                        || monoVertical(board, cell))) {
-                    result = true;
-                    break;
-                }
+            if (board[row][row] == 1 && (monoHorizontal(board, row)
+                        || monoVertical(board, row))) {
+                result = true;
+                break;
             }
         }
         return result;
@@ -39,5 +37,3 @@ public class Win {
         return result;
     }
 }
-
-
